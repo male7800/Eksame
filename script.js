22b6c5518aa8a65963e4f6f092327cb2d06d0561
Malene Lamb <male7800@stud.kea.dk>
2021-06-11 10:51:44 +0200

    new

--- script.js
+++ script.js
@@ -17,39 +17,3 @@ function toggleMenu() {
     document.querySelector("#menuknap").textContent = "X";
   }
 }
-
-class Login extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({
-      mode: "open",
-    }); //apparently slots only work with the shadow dom?
-  }
- 
-
-    this.shadowRoot.querySelector("form").addEventListener("submit", (e) => {
-      e.preventDefault();
-      if (this.shadowRoot.querySelector("input[name=pass]").value === "kea") {
-        document.querySelector("#totally-delete-me").remove();
-        localStorage.setItem("iform-totally-logged-in", true);
-      }
-    });
-  }
-  render() {
-    this.shadowRoot.innerHTML = this.html;
-  }
-}
-customElements.define("iform-login", Login);
-window.addEventListener("load", () => {
-  if (!localStorage.getItem("iform-totally-logged-in")) {
-    const div = document.createElement("div");
-    div.id = "totally-delete-me";
-    div.style.width = "100vw";
-    div.style.height = "100vh";
-    div.style.position = "fixed";
-    div.style.zIndex = "9999";
-
-    div.innerHTML = "<iform-login />";
-    document.body.prepend(div);
-  }
-});
